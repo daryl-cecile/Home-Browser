4669b6eaa7e70c0e44fbe70539c573cd3709105b
darylcecile <darylcecile@gmail.com>
2018-03-04 05:30:23 +0000

    clean up
    Removed release objects

--- release/HomeBrowser-darwin-x64/HomeBrowser.app/Contents/Resources/app/rsrc/system/settings.js
+++
@@ -1,8 +0,0 @@
-var HomeBrowser;
-(function (HomeBrowser) {
-    function show() {
-        require('electron').remote.getGlobal('showWindow')();
-    }
-    HomeBrowser.show = show;
-})(HomeBrowser || (HomeBrowser = {}));
-//# sourceMappingURL=settings.js.map
